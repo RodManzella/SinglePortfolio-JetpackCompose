87998f385788a02a19f663f4be242dbda67c9dbb
RodManzella <manzellateixeira@gmail.com>
2023-12-04 21:43:54 -0300

    contatos

--- app/src/main/java/com/example/dugs_jetpackcompose/screens/Contato.kt
+++ app/src/main/java/com/example/dugs_jetpackcompose/screens/Contato.kt
@@ -1,23 +1,63 @@
 package com.example.dugs_jetpackcompose.screens
 
-import androidx.compose.foundation.layout.Box
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Contato(){
-    Box(
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center){
-        Text(text = "Contato",
-            fontFamily = FontFamily.Serif,
-            fontSize = 22.sp
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Minhas Redes Sociais",
+            modifier = Modifier
+                .padding(15.dp)
+
         )
+        MyButton("Linkedin", "https://www.linkedin.com/in/rodrigo-manzella-7b8b0a23b/")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyButton("GitHub", "https://github.com/RodManzella")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyButton("Instagram", "https://www.instagram.com/rodrigo_manzella/?hl=en")
     }
+}
+
+@Composable
+fun MyButton(label: String, url: String) {
+    val context = LocalContext.current
+
+    Button(
+        onClick = {
+
+            openUrlInBrowser(context, url)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text(text = label)
+    }
+}
+
+fun openUrlInBrowser(context: android.content.Context, url: String) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    context.startActivity(intent)
 }
